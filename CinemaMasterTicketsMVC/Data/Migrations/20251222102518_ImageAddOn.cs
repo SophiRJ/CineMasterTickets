ebcd0b3fb1180ba1f74063d13c23ab0784453844
SophiRJ <sofiarojasc5@gmail.com>
2025-12-22 18:19:12 +0100

    Panel Admnistrador, Gestion de Adons

---
+++ CinemaMasterTicketsMVC/Data/Migrations/20251222102518_ImageAddOn.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace CinemaMasterTicketsMVC.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class ImageAddOn : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "AddOnImage",
+                table: "AddOns",
+                type: "nvarchar(max)",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "AddOnImage",
+                table: "AddOns");
+        }
+    }
+}
